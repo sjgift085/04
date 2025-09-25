bb678755db581d026d76fa9dfc4177f0dc5f04a6
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-09-25 15:20:06 +0900

    실습 4

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 int main(int argc, char *argv[])
 {
   
-  int sec;
+  int year;
   
-  printf("input the second :");
-  scanf("%i", &sec);
+  printf("input the year : ");
+  scanf("%i", &year);
 
-  printf("the time is %i : %i\n", sec/60, sec%60);
+  printf("is the year %i the leap year? : %i\n", year, (year%4 == 0 && year%100 != 0) || year%400 == 0);
   
   
   system("PAUSE");	
